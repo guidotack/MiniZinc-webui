7805703a851d265687e8c96190e4635fb222a78e
Sam Wright <s.wright.aus@gmail.com>
2016-10-11 22:33:53 +1100

    Added scatter plot output type

--- src/Outputs/Outputs.js
+++ src/Outputs/Outputs.js
@@ -1,14 +1,14 @@
 import { OutputMatrix1D,OutputMatrix2D } from './OutputMatrix';
-import { OutputGanttChart } from './OutputGanttChart';
+import { OutputScatterPlot } from './OutputScatterPlot';
 
 export const AllOutputs = [
     { component: "OutputMatrix1D", name: "Matrix1D", type: "int-1D", image: "bars" },
     { component: "OutputMatrix2D", name: "Matrix2D", type: "int-2D", image: "table" },
-    { component: "OutputGanttChart", name: "GanttChart", type: "int-2D", image: "calendar" },
+    { component: "OutputScatterPlot", name: "Scatter Plot", type: "int-2D", image: "dot-circle-o" },
 ]
 
 export const OutputStringToComponent = {
     OutputMatrix1D: OutputMatrix1D,
     OutputMatrix2D: OutputMatrix2D,
-    OutputGanttChart: OutputGanttChart
+    OutputScatterPlot: OutputScatterPlot
 }
