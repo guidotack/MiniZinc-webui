88b7050699a8678f452c99449ef2f9673b02ba52
Kevin Imvoo <Imvooz@gmail.com>
2016-10-13 17:46:17 +1100

    I'm actually so confused where these went after af8066ad LOL.

--- src/Outputs/Outputs.js
+++ src/Outputs/Outputs.js
@@ -1,16 +1,22 @@
 import { OutputMatrix1D,OutputMatrix2D } from './OutputMatrix';
 import { OutputScatterPlot,OutputGanttChart } from './OutputCharts';
+import { OutputBox } from './OutputBox';
 
 export const AllOutputs = [
     { component: "OutputMatrix1D", name: "Matrix1D", type: "int-1D", image: "bars" },
     { component: "OutputMatrix2D", name: "Matrix2D", type: "int-2D", image: "table" },
     { component: "OutputScatterPlot", name: "Scatter Plot", type: "int-2D", image: "dot-circle-o" },
     { component: "OutputGanttChart", name: "Gantt Chart", type: "int-2D", image: "signal" },
-]
+    { component: "OutputBox", name: "OutputBox", type: "int" },
+    { component: "OutputBox", name: "OutputBox", type: "string" }
+];
 
 export const OutputStringToComponent = {
+    OutputBox: OutputBox,
     OutputMatrix1D: OutputMatrix1D,
     OutputMatrix2D: OutputMatrix2D,
     OutputScatterPlot: OutputScatterPlot,
     OutputGanttChart: OutputGanttChart,
 }
+
+af8066
